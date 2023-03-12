8ed2c96ffdcdb2d92ab8262b2ef9f9d818cffc8d
Mark Botros <botrosmark200@gmail.com>
2023-03-12 16:06:33 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An LSTM-based Recurrent Neural Network that translates Modern English into Old (Shakespearean) English',
+    author='Mark Botros',
+    license='MIT',
+)
